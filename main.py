81ef376fe57d86782028b67d65f31af254cc0441
Nikolay <80151381+Fresspers@users.noreply.github.com>
2022-09-15 10:07:41 +0500

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-#from random import *
-#а1  =  0
-#f = open ("BD/BD1.txt","w")
-#f.write ("Штат Аризона, граничит с Мексикой.")
-#f.close
-#for i in range(20):
-    #f = open(f"{i}","w")
-    #f.write(е"{randint(0,100)}")
-    #f.close
